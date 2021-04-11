c259f4e9dc828e2dec5e37cbb9b8fe073fce066b
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-04-11 13:53:37 +0530

    Create Work Break.cs

---
+++ DP/Work Break.cs
@@ -0,0 +1,44 @@
+//Given a string A and a dictionary of words B, determine if A can be segmented into a space-separated sequence of one or more dictionary words.
+//Input 1:
+//A = "myinterviewtrainer",B = ["trainer", "my", "interview"]   Output:1
+
+//not an optimal soln
+class Solution {
+    public int wordBreak(string A, List<string> B) {
+        
+        int n = A.Length;
+        
+        HashSet<string> hash = new HashSet<string>();
+        foreach(var item in B){
+            hash.Add(item);
+        }
+         
+        bool[,] dp = new bool[n,n];
+        //length from 1 to n
+        for(int l=1;l<=n;l++){ 
+            for(int i=0;i<=n-l;i++){
+                int j = i+l-1;
+                string sub = A.Substring(i,l);//ex:substring of length 3 from 0th index => (0,3);  
+                if(hash.Contains(sub)){
+                    dp[i,j]= true;
+                }
+                else{
+                    //find a k between i+1 to j such that T[i][k-1] && T[k][j] are both true 
+                for(int k=i+1; k <= j; k++){
+                    if(dp[i,k] != false && dp[k+1,j] != false){
+                        dp[i,j] = true;
+                        break;
+                    }
+                }   
+                } 
+            }   
+        }  
+       if(dp[0,n-1]){
+           return 1;
+       } 
+       else{
+           return 0;
+       }
+    }
+}
+
