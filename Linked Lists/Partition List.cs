d82b9a3bf90a1bb07519ff4716eb764fc4fdddc2
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-04-22 00:05:15 +0530

    Create Partition List.cs

---
+++ Linked Lists/Partition List.cs
@@ -0,0 +1,62 @@
+/*Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+You should preserve the original relative order of the nodes in each of the two partitions.
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {this.val = x; this.next = null;}
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int B) {
+        
+        ListNode smallhead = new ListNode();
+        ListNode bighead = new ListNode();
+        ListNode smalltemp = new ListNode();
+        ListNode bigtemp = new ListNode(); 
+        bool smallheadfound = false;
+        bool bigheadfound = false;
+        while(head!=null){
+            
+            if(head.val < B){
+               if(!smallheadfound){
+                   
+                   smallhead = head;
+                   smalltemp = head;
+                   smallheadfound = true;
+               } 
+               else{
+                   smalltemp.next=head;
+                   smalltemp =smalltemp.next;
+               } 
+            }
+            else{
+                
+                if(!bigheadfound){ 
+                   bighead = head;
+                   bigtemp = head;
+                   bigheadfound = true;
+               } 
+               else{
+                   bigtemp.next=head;
+                   bigtemp = bigtemp.next;
+               } 
+            } 
+           head=head.next; 
+        }
+        //when only one type of elements are there i.e., either lesser than x or vice versa
+        if(!bigheadfound){return smallhead;}
+        if(!smallheadfound){return bighead;}
+        
+        smalltemp.next = bighead;
+        bigtemp.next= null;
+        return smallhead; 
+    }
+}
+
+//or take two emmpty nodes , keep on adding smaller and greater elements, merge them and return
