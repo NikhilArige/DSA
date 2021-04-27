287769745b709c15979f49bfd987b6dbefdad8f3
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-04-27 16:59:07 +0530

    Create Sorted Insert Position.cs

---
+++ Searching&Sorting/Sorted Insert Position.cs
@@ -0,0 +1,43 @@
+/*Given a sorted array A and a target value B, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You may assume no duplicates in the array.
+**Example Input**
+Input 1:
+A = [1, 3, 5, 6]
+B = 5
+Input 2:
+A = [1, 3, 5, 6]
+B = 2
+**Example Output**
+Output 1: 2
+Output 2: 1
+**Example Explanation**
+Explanation 1:
+5 is found at index 2.
+Explanation 2:
+2 will be inserted ar index 1. */
+
+
+
+public class Solution {
+	public int searchInsert(List<int> a, int b) {
+	    
+	    int n = a.Count;
+	    int l = 0,r = n-1;
+	    while(l<=r){
+	      
+	      int mid = (l+r)/2;
+	      
+            if(a[mid]==b){
+              return mid;  
+            } 
+            else if(a[mid]<b){
+              l = mid+1;  
+            } 
+            else{
+             r = mid-1;   
+            }    
+	    }
+	    //when l<=r, l will be at the position where b could be
+	    return l;
+	}
+}
