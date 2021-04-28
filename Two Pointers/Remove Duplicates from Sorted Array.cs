8b430e33bc8e0604c08340e6f378cc840ddb2fca
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-04-28 17:07:18 +0530

    Create Remove Duplicates from Sorted Array.cs

---
+++ Two Pointers/Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,32 @@
+/* Remove duplicates from Sorted Array
+Given a sorted array, remove the duplicates in place such that each element appears only once and return the new length.
+Note that even though we want you to return the new length, make sure to change the original array as well in place
+Do not allocate extra space for another array, you must do this in place with constant memory.
+Example:
+Given input array A = [1,1,2],
+Your function should return length = 2, and A is now [1,2]. */
+
+public class Solution {
+	public int removeDuplicates(List<int> a) {
+	    
+	    int n = A.Count;
+	    if(n==1){
+	        return 1;
+	    } 
+	    int i = 0;
+	    for(int j = i+1;j<n;j++){
+	        
+	        if(A[i]!=A[j]){
+	            i++;
+	            swap(A,i,j);
+	        } 
+	    }
+	    return i+1;
+	}
+	
+	private void swap(List<int> A,int i,int j){ 
+	    int temp = A[i];
+	    A[i] = A[j];
+	    A[j] = temp; 
+	}
+}
