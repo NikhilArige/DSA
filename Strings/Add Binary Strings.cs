1f5236c6a801d28c3968b998ddd7c17c7a5c39e9
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-05-19 23:50:01 +0530

    Update Add Binary Strings.cs

--- Strings/Add Binary Strings.cs
+++ Strings/Add Binary Strings.cs
@@ -4,7 +4,7 @@ a = "100"
 b = "11"
 Return a + b = “111”.*/
 
-
+//getting TLE for this
 using System.Text;
 class Solution {
     public string addBinary(string A, string B) {
@@ -58,4 +58,72 @@ class Solution {
     }
 }
 
+//Got no TLE with this
+using System.Text;
+using System.Linq;
+class Solution {
+    public string addBinary(string A, string B) {
+            
+            A = new string(A.ToCharArray().Reverse().ToArray());
+            B = new string(B.ToCharArray().Reverse().ToArray());
+           int m = A.Length;
+           int n = B.Length;
+           int dif = 0;
+           if(m>n){
+               dif = m-n;
+               string s= "";
+               for(int i=0;i<dif;i++){
+                   s+="0";
+               }
+               B+=s;
+           }
+           else if(n>m){
+               dif = n-m;
+               string s= "";
+               for(int i=0;i<dif;i++){
+                   s+="0";
+               }
+               A+=s; 
+           }
+           StringBuilder result = new StringBuilder();
+           
+           char ch = '0';
+           for(int i=0;i<A.Length;i++){
+               
+               if(ch == '1' && (A[i]=='1' && B[i]=='1')){
+                   
+                   result.Append("1");
+                   continue;
+               }
+               else if(A[i]=='1' && B[i]=='1' && ch=='0'){ 
+                   result.Append("0");
+                   ch = '1';
+                   continue;
+               }
+               else if(A[i]=='0' && B[i]=='0' && ch=='0'){
+                   result.Append("0");
+                   continue;
+               }
+               else if(A[i]=='0' && B[i]=='0' && ch=='1'){
+                   result.Append("1");
+                   ch = '0';
+                   continue;
+               }
+               else if(((A[i]=='1' && B[i]=='0')||(A[i]=='0' && B[i]=='1')) && ch=='1'){
+                   result.Append("0"); 
+                   continue;
+               }
+               else if(((A[i]=='1' && B[i]=='0')||(A[i]=='0' && B[i]=='1')) && ch=='0'){
+                   result.Append("1");
+                   continue;
+               } 
+           }
+           if(ch=='1'){
+               result.Append("1");
+           } 
+          string res = result.ToString();
+           res = new string(res.ToCharArray().Reverse().ToArray()); 
+           return res;    
+    }
+}
 
