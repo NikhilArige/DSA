c06f4c014f55f9ad86d6e2122a2c51d0a3caafd9
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-08-28 15:26:43 +0530

    Update Trailing Zeros in Factorial.cs

--- Math/Trailing Zeros in Factorial.cs
+++ Math/Trailing Zeros in Factorial.cs
@@ -14,3 +14,20 @@ class Solution {
         return cnt;
     }
 }
+
+
+
+public class Solution {
+
+    public int TrailingZeroes(int n) {
+        
+       int count = 0;
+        
+        while(n > 0){
+            count += n / 5;
+            n /= 5;
+        }
+        
+        return count;
+    }
+}
