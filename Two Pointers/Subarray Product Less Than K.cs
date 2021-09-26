08aca3b3587858a1ea876753e6db9d5d4fd2df75
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-09-26 12:54:37 +0530

    Create Subarray Product Less Than K.cs

---
+++ Two Pointers/Subarray Product Less Than K.cs
@@ -0,0 +1,31 @@
+/*
+Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr]
+of which the sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+Example 1:
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+Input: target = 4, nums = [1,4,4]
+Output: 1
+*/
+
+public class Solution {
+    public int NumSubarrayProductLessThanK(int[] nums, int k) {
+        int cnt = 0,prod = 1,start=0; 
+        for(int i = 0; i < nums.Length; i++)
+        {
+            prod *= nums[i];
+            while(start < i && prod >= k)
+            {
+                prod /= nums[start];
+                start++;
+            }
+            if(prod < k)
+            {
+                cnt += (i - start + 1);
+            }
+        }
+        return cnt;
+    }
+}
