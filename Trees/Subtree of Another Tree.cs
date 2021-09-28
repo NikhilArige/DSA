7b1b4b505150c49224148d6cad4d576eb18952ba
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-09-28 22:00:04 +0530

    Update Subtree of Another Tree.cs

--- Trees/Subtree of Another Tree.cs
+++ Trees/Subtree of Another Tree.cs
@@ -57,3 +57,34 @@ public class Solution {
         return false;
     }
 }
+
+
+
+public class Solution {
+    
+    public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+        
+        if(root == null) return false;       
+        
+        else if((root.val == subRoot.val)&& IsSameTree(root, subRoot)) return true;
+        
+        else
+        {
+            return IsSubtree(root.left, subRoot) 
+                || IsSubtree(root.right, subRoot);
+        }
+               
+    }
+    
+    private bool IsSameTree(TreeNode root, TreeNode subRoot)
+    {
+        if(root == null && subRoot== null ) return true;
+        
+        else if(root == null || subRoot == null) return false;
+        
+        
+        return (root.val == subRoot.val) 
+                && IsSameTree(root.left, subRoot.left) 
+                && IsSameTree(root.right, subRoot.right);
+    }
+}
