bf0075a9bf3bee4d756ce1b8a623d7fcd464113b
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-10-06 15:09:58 +0530

    Update Integer To Roman.cs

--- Strings/Integer To Roman.cs
+++ Strings/Integer To Roman.cs
@@ -108,3 +108,32 @@ class Solution {
 }
 
     
+var symList = new List<KeyValuePair<int, string>>()
+		{
+			 new KeyValuePair<int, string>(1000, "M"),
+			 new KeyValuePair<int, string>(900, "CM"),
+			 new KeyValuePair<int, string>(500, "D"),
+			 new KeyValuePair<int, string>(400, "CD"),
+			 new KeyValuePair<int, string>(100, "C"),
+			 new KeyValuePair<int, string>(90, "XC"),
+			 new KeyValuePair<int, string>(50, "L"),
+			 new KeyValuePair<int, string>(40, "XL"),
+			 new KeyValuePair<int, string>(10, "X"),
+			 new KeyValuePair<int, string>(9, "IX"),
+			 new KeyValuePair<int, string>(5, "V"),
+			 new KeyValuePair<int, string>(4, "IV"),
+			 new KeyValuePair<int, string>(1, "I")
+		};
+
+		string result = "";
+
+		foreach (var item in symList)
+		{
+			while (num>=item.Key)
+			{
+				result += item.Value;
+				num -= item.Key;
+			}
+		}
+
+		return result;
