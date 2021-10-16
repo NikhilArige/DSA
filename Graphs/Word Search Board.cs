401a242bba7e67005b79a878a5a5019739e9a966
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2021-10-17 00:11:41 +0530

    Update Word Search Board.cs

--- Graphs/Word Search Board.cs
+++ Graphs/Word Search Board.cs
@@ -60,3 +60,49 @@ class Solution {
         }   
     }
 }
+
+
+
+
+public class Solution {
+    public bool Exist(char[][] board, string word) {
+        if(board ==null || board.Length<0){
+             return false;
+        }
+        var rows = board.Length;
+        var cols = board[0].Length;
+        
+        var visited = new bool[rows,cols];
+        
+        for(int r=0;r<rows;r++){
+             for(int c= 0;c<cols;c++){
+                 if(board[r][c] == word[0]){
+                     if(DFS(board,word,visited,rows,cols,r,c,0) ){
+                        return true;
+                   }
+                 }
+             }
+        }
+        return false;
+    }
+    
+    public static bool DFS(char[][] board, string word, bool[,] visited, int rows, int cols, int r, int c, int level=0)
+    { 
+        if(level==word.Length){
+            return true; 
+        }
+        
+        if((r<0 || r>=rows) || (c<0||c>=cols) || visited[r,c] || board[r][c]!=word[level] ){
+            return false;
+        } 
+        visited[r,c]=true;
+        if(DFS(board,word,visited,rows,cols,r-1,c,level+1)) return true;
+        if(DFS(board,word,visited,rows,cols,r+1,c,level+1)) return true;
+        if(DFS(board,word,visited,rows,cols,r,c-1,level+1)) return true;
+        if(DFS(board,word,visited,rows,cols,r,c+1,level+1)) return true;
+        
+        visited[r,c]=false;
+        
+        return false;
+    }
+}
