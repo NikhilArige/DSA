481d083f77bae7b8a2e662ca133e35ab007b38c5
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2022-01-14 20:07:07 +0530

    Update Multiply Strings.cs

--- Strings/Multiply Strings.cs
+++ Strings/Multiply Strings.cs
@@ -77,3 +77,36 @@ class Solution {
             return s;       
     }
 }
+
+
+
+
+
+
+
+
+public class Solution {
+    public string Multiply(string num1, string num2) {
+        if(num1 == "0" || num2 == "0"){
+            return "0";
+        }
+        int m = num1.Length,n = num2.Length;
+        var res = new int[m+n];
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){ 
+                int prod = (num1[i]-'0')*(num2[j]-'0');
+                var sum = res[i+j+1] + prod;
+                res[i+j+1] = sum%10;
+                res[i+j] += sum/10; 
+            }
+        }
+        var sb = new StringBuilder();
+        for(int k=0;k<m+n;k++){
+            if(k==0 && res[k]==0){
+                continue;
+            }
+            sb.Append(res[k]);
+        }
+        return sb.ToString();
+    }
+}
