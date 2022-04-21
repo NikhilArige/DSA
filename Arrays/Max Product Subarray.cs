5aba0f0ad1cfe3dfaa0412624acdcafdf54e6351
NikhilArige <57755101+NikhilArige@users.noreply.github.com>
2022-04-21 15:19:40 +0530

    Update Max Product Subarray.cs

--- Arrays/Max Product Subarray.cs
+++ Arrays/Max Product Subarray.cs
@@ -64,3 +64,18 @@ public class Solution {
         return res;
     }
 }
+
+public class Solution {
+    public int MaxProduct(int[] nums) {
+        
+        int res = nums[0],l=0,r=0;
+        for(int i=0;i<nums.Length;i++){
+            
+            l = (l==0 ? 1 : l) * nums[i];
+            r = (r==0 ? 1 : r) * nums[nums.Length-1 -i];
+            res = Math.Max(res,Math.Max(l,r));
+        }
+        
+        return res;
+    }
+}
